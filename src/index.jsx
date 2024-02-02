ea7849674549a2615dddf343bac7a5c31fbbbb0a
Jerel Miller <jerelmiller@gmail.com>
2024-02-02 14:24:26 -0700

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,73 @@
+/*** APP ***/
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+  useLazyQuery
+} from "@apollo/client";
+import { GraphQLID } from "graphql";
+
+import { link } from "./link.js";
+import { Subscriptions } from "./subscriptions.jsx";
+import { Layout } from "./layout.jsx";
+import "./index.css";
+
+const ALL_PEOPLE = gql`
+  query AllPeople ($id: ${GraphQLID}) {
+    people (id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+let counterFn = 0;
+let counterQuery = 0;
+
+function App() {
+  const [getData] = useLazyQuery(ALL_PEOPLE, {
+    onCompleted(data) {
+      console.log("COMPLETED QUERY: ", ++counterQuery);
+    }
+  });
+
+  useEffect(() => {
+    console.log("USE FUNCTION: ", ++counterFn);
+    getData({
+      variables: {
+        id: 1
+      }
+    });
+    setTimeout(() => {
+      console.log("USE FUNCTION: ", ++counterFn);
+      getData();
+    }, 3000);
+  }, [getData]);
+
+  return <main></main>;
+}
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link
+});
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <ApolloProvider client={client}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="subscriptions-wslink" element={<Subscriptions />} />
+        </Route>
+      </Routes>
+    </Router>
+  </ApolloProvider>
+);
---
+++ src/schema.js
@@ -0,0 +1,39 @@
+/*** SCHEMA ***/
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList
+} from "graphql";
+
+const PersonType = new GraphQLObjectType({
+  name: "Person",
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString }
+  }
+});
+
+const peopleData = [
+  { id: 1, name: "John Smith" },
+  { id: 2, name: "Sara Smith" },
+  { id: 3, name: "Budd Deey" }
+];
+
+const QueryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    people: {
+      type: new GraphQLList(PersonType),
+      args: {
+        id: { type: GraphQLID }
+      },
+      resolve: function (_, arg) {
+        return peopleData;
+      }
+    }
+  }
+});
+
+export const schema = new GraphQLSchema({ query: QueryType });
